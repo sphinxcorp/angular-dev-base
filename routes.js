22d0e057f09106f71179ad532cadce4e7e440566
M N Islam Shihan <mnishihan@gmail.com>
2013-12-24 13:53:34 +0600

    fixed app starting issue in node connect service

--- routes.js
+++ routes.js
@@ -32,8 +32,21 @@
 
   module.exports = function(app, options) {
     app.get('/', function(req, res) {
+      return res.redirect("/desktop");
+    });
+
+    app.get('/desktop', function(req, res) {
       return res.render("" + options.base + "/desktop/index.html");
     });
+
+    app.get('/phone', function(req, res) {
+      return res.render("" + options.base + "/phone/index.html");
+    });
+
+    app.get('/tablet', function(req, res) {
+      return res.render("" + options.base + "/phone/index.html");
+    });
+
     app.get('/people', function(req, res) {
       return res.json(people);
     });
